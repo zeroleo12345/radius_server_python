acd96a3047835c69d1ec895f3457a27fed9fb4ac
zeroleo12345 <zeroleo12345@163.com>
2018-10-15 21:13:27 +0800

    use base.sh

--- bin/acct.sh
+++ bin/acct.sh
@@ -2,10 +2,5 @@
 
 project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
 
-pip3 install -r $project_root/requirements/requirements.txt
-
-export PYTHONPATH=$project_root/src:$PYTHONPATH
-export DICTIONARY_DIR=$project_root/dictionary
-export SECRET="testing123"
-export USER_DB=$project_root/data/users.db
+source $project_root/bin/base.sh
 python3 $project_root/src/acct/processor.py
---
+++ bin/base.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
+
+pip3 install -r $project_root/requirements/requirements.txt
+
+export PYTHONPATH=$project_root/src:$PYTHONPATH
+export DICTIONARY_DIR=$project_root/dictionary
+export SECRET="testing123"
+export USER_DB=$project_root/data/users.db
