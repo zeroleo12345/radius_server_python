99a14ae1cb092c4887f439c8262862ce5d5e2185
zeroleo12345 <zeroleo12345@163.com>
2018-10-16 11:15:13 +0800

    add opkg install python3-cryptography

--- bin/base.sh
+++ bin/base.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
 
+opkg install python3-cryptography
 pip3 install -r $project_root/requirements/requirements.txt
 
 export PYTHONPATH=$project_root/src:$PYTHONPATH
