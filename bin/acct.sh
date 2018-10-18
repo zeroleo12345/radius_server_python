763666100cb9b67b94f2c31cf492a1f6267cfeed
zeroleo12345 <zeroleo12345@163.com>
2018-10-18 18:09:40 +0800

    增加exec, 用以替换sh进程

--- bin/acct.sh
+++ bin/acct.sh
@@ -4,4 +4,4 @@ project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
 
 export LOG_HEADER="acct"
 source $project_root/bin/env.sh
-python3 $project_root/src/acct/processor.py
+exec python3 $project_root/src/acct/processor.py
--- bin/auth.sh
+++ bin/auth.sh
@@ -4,4 +4,4 @@ project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
 
 export LOG_HEADER="auth"
 source $project_root/bin/env.sh
-python3 $project_root/src/auth/processor.py
+exec python3 $project_root/src/auth/processor.py
