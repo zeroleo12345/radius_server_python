8c6e8e50c10a90aa889332b1d364685173527919
zeroleo12345 <zeroleo12345@163.com>
2018-11-23 22:09:24 +0800

    modify LOG_HEADER

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -2,6 +2,6 @@
 
 project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
 
-export LOG_HEADER="user"
+export LOG_HEADER="monitor"
 source $project_root/bin/env.sh
 exec python3 $project_root/src/task/process_monitor.py
