4ae3d2a2e43df51c8a402acb60da442aa2f5f040
zeroleo12345 <zeroleo12345@163.com>
2018-11-24 10:37:09 +0800

    [ADDED] 修改环境变量

--- bin/acct.sh
+++ bin/acct.sh
@@ -3,5 +3,5 @@
 project_root=$(cd "$(dirname "$0")/.."; pwd)    # /root/radius_server
 
 export LOG_HEADER="acct"
-source $project_root/bin/env.sh
+source $project_root/.env
 exec python3 $project_root/src/acct/processor.py
