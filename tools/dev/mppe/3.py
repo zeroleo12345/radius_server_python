0989a6d26c0691a41a2d16cb4e28b3bff257bb0e
zeroleo12345 <zeroleo12345@163.com>
2020-11-02 22:48:59 +0800

    add

--- tools/dev/mppe/3.py
+++ tools/dev/mppe/3.py
@@ -8,11 +8,13 @@ def _create_plain_text(key) -> bytes:
     key_len = len(key)
     while (len(key) + 1) % 16:
         key += b'\0'
-    return chr(key_len).encode() + key
+    return bytes([key_len]) + key
 
 
-def _create_salt():
-    return chr(128 + random.randrange(0, 128)).encode() + chr(random.randrange(0, 256)).encode()
+def _create_salt() -> bytes:
+    r = bytes([128 + 100]) + bytes([100])
+    return r
+    #  return bytes([128 + random.randrange(0, 128)]) + bytes([random.randrange(0, 256)])
 
 
 def _create_send_salt_recv_salt():
@@ -23,11 +25,11 @@ def _create_send_salt_recv_salt():
     return send_salt, recv_salt
 
 
-def _xor(str1, str2):
-    return ''.join(map(lambda s1, s2: chr(ord(s1) ^ ord(s2)), str1, str2))
+#  def _xor(str1, str2):
+    #  return ''.join(map(lambda s1, s2: bytes([ord(s1) ^ ord(s2)]), str1, str2))
 
 
-def _bxor(b1, b2):
+def _xor(b1, b2):
     import operator
     return bytes(map(operator.xor, b1, b2))
 
@@ -67,28 +69,34 @@ def create_mppe_recv_key_send_key(msk, secret, authenticator):
 
 
 if __name__ == "__main__":
+    msk = b"\xbb'}!k\xa4\x98\xdf\xf7\xc1\xbc\x1e\xb9\xd3s}\xcb\rT,\xf5\xad\xb0g\x85\x85\x10\x91s0\xc3\xc3\xfa\xa3\x05\xcc\xd2\xa0\x1c!\x90\xc7E\xad\xc9\x163\x98\xbd\xe4\x15h\xe0\xf9\xc6x\xbb\x9d\xf6\xf79a\xa7\x04"
+
+    secret = b'testing123'
+
+    authenticator = b'g\nph\x9d4U\x89\xa7 \xfb3gm^\xda'
+
     def print_b64(**kwargs):
         import base64
         for k, v in kwargs.items():
+            print('repr({k}) = {v}'.format(k=k, v=repr(v)))
             print('{k}: {v}'.format(k=k, v=base64.b64encode(v).decode()))
+            print('')
 
-    def test_all():
-        msk = b"\xbb'}!k\xa4\x98\xdf\xf7\xc1\xbc\x1e\xb9\xd3s}\xcb\rT,\xf5\xad\xb0g\x85\x85\x10\x91s0\xc3\xc3\xfa\xa3\x05\xcc\xd2\xa0\x1c!\x90\xc7E\xad\xc9\x163\x98\xbd\xe4\x15h\xe0\xf9\xc6x\xbb\x9d\xf6\xf79a\xa7\x04"
-
-        secret = b'testing123'
-
-        authenticator = b'g\nph\x9d4U\x89\xa7 \xfb3gm^\xda'
-
-        recv_key, send_key = create_mppe_recv_key_send_key(msk=msk, secret=secret, authenticator=authenticator)
-
-        import base64
-        print_b64(recv_key=recv_key)
-        print_b64(send_key=send_key)
+    def test_create_plain_text():
+        mppe_send_key, mppe_recv_key = (msk[32:], msk[0:32])
+        send_text, recv_text = map(_create_plain_text, (mppe_send_key, mppe_recv_key))
+        print_b64(send_text=send_text)
+        print_b64(recv_text=recv_text)
 
     def test_create_salt():
         recv_salt = _create_salt()
         print_b64(recv_salt=recv_salt)
 
+    def test_create_mppe_recv_key_send_key():
+        recv_key, send_key = create_mppe_recv_key_send_key(msk=msk, secret=secret, authenticator=authenticator)
+        print_b64(recv_key=recv_key)
+        print_b64(send_key=send_key)
+    #
     import sys
     func_name = sys.argv[1]
     eval(func_name)()
