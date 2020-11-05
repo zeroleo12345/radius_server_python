02b1ce37aa08dad1b7eea3bf3275325ac710f264
zeroleo12345 <zeroleo12345@163.com>
2020-11-05 21:30:00 +0800

    m

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,8 +3,10 @@ from dynaconf import Dynaconf
 
 class Config(object):
     _settings = Dynaconf(
-        envvar_prefix='',
+        load_dotenv=True,       # 是否读取.env
         dotenv_path='/app/.env',
+        envvar_prefix=False,    # 变量变成是否需前缀
+        dotenv_override=False,  # 设置.env配置是否覆盖环境变量
     )
 
     def __call__(self, key, default=None, cast=None, fresh=False, dotted_lookup=True, parent=None):
