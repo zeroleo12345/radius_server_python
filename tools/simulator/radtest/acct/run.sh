6aea97d405a8cf91428514165e7107f2a78ac613
zeroleo12345 <zeroleo12345@163.com>
2020-11-14 20:19:17 +0800

    m

--- tools/simulator/radtest/acct/run.sh
+++ tools/simulator/radtest/acct/run.sh
@@ -1,1 +1,1 @@
-radclient -D /app/tools/simulator/etc/dictionary -d /app/etc/dictionary 127.0.0.1:1813  acct  'testing123'  < /app/tools/simulator/acct/i.conf
+radclient -D /app/tools/simulator/etc/dictionary -d /app/etc/dictionary 127.0.0.1:1813  acct  'testing123'  < /app/tools/simulator/radtest/acct/i.conf
--- tools/simulator/radtest/auth/run.sh
+++ tools/simulator/radtest/auth/run.sh
@@ -1,1 +1,1 @@
-radclient radclient -D /app/tools/simulator/etc/dictionary -d /app/etc/dictionary 127.0.0.1:1812  auth  'testing123'  < /app/tools/simulator/auth/chap.conf
+radclient radclient -D /app/tools/simulator/etc/dictionary -d /app/etc/dictionary 127.0.0.1:1812  auth  'testing123'  < /app/tools/simulator/radtest/auth/chap.conf
