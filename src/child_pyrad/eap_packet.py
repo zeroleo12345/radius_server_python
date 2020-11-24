c777f823e58775610d46c82b577a8bb0e4e5d9ab
zeroleo12345 <zeroleo12345@163.com>
2020-11-24 23:57:43 +0800

    fix

--- src/child_pyrad/eap_packet.py
+++ src/child_pyrad/eap_packet.py
@@ -23,7 +23,7 @@ class EapPacket(Eap):
     |     Code      |  Identifier   |            Length             |
     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     """
-    def __init__(self, content: bytes = None, code: int = 0, id: int = 0, type: int = None, type_data: bytes = b''):
+    def __init__(self, code: int = 0, id: int = 0, type: int = None, type_data: bytes = b''):
         assert isinstance(type_data, bytes)
 
         self.code = code            # int 1-byte
@@ -35,23 +35,19 @@ class EapPacket(Eap):
         else:
             self.type = None            # int 1-byte
             self.type_data = b''       # binary
-        if content is not None:
-            self.decode_packet(content)
-        else:
-            # write mode
-            # if self.type_data == '': raise PacketError('type_data missing')
-            pass
 
-    def decode_packet(self, packet: bytes):
+    @classmethod
+    def decode_packet(cls, packet: bytes) -> 'EapPacket':
         try:
-            self.code, self.id, _length = struct.unpack("!2BH", packet[:4])
+            code, id, _length = struct.unpack("!2BH", packet[:4])
         except struct.error:
             raise PacketError('EAP header is corrupt')
         if len(packet) != _length:
             raise PacketError('EAP has invalid length')
-        if self.code in [Eap.CODE_EAP_REQUEST, Eap.CODE_EAP_RESPONSE]:
-            self.type, = struct.unpack("!B", packet[4:5]) if _length > 4 else None
-            self.type_data = packet[5:_length] if _length > 5 else ''
+        assert code in [Eap.CODE_EAP_REQUEST, Eap.CODE_EAP_RESPONSE]
+        type, = struct.unpack("!B", packet[4:5]) if _length > 4 else None
+        type_data = packet[5:_length] if _length > 5 else ''
+        return EapPacket(code=code, id=id, type=type, type_data=type_data)
 
     def pack(self):
         if self.code in [Eap.CODE_EAP_REQUEST, Eap.CODE_EAP_RESPONSE]:
