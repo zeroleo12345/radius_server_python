f4fea343f59975440985435fc2a02f86e78715b0
zeroleo12345 <zeroleo12345@163.com>
2020-11-25 23:57:57 +0800

    m

--- tools/simulator/eap_test/run.sh
+++ tools/simulator/eap_test/run.sh
@@ -1,1 +1,1 @@
-eapol_test -c /app/tools/simulator/eap_test/eapol_test.conf.peap_v1_gtc -a 127.0.0.1 -p 1812 -s testing123 -r 0
+eapol_test -a 127.0.0.1 -p 1812 -s testing123 -r 0 -c /app/tools/simulator/eap_test/eapol_test.conf.peap_gtc
