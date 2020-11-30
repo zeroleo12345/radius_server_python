2db660d3331f79b94859c5410c08a96ef25d34de
zeroleo12345 <zeroleo12345@163.com>
2020-12-01 00:05:44 +0800

    m

--- src/auth/eap_peap_mschapv2_flow.py
+++ src/auth/eap_peap_mschapv2_flow.py
@@ -411,12 +411,14 @@ class EapPeapMschapv2Flow(Flow):
     def access_accept(cls, request: AuthRequest, session: EapPeapSession):
         log.info(f'OUT: accept|EAP-PEAP|{request.username}|{session.auth_user.inner_username}|{request.mac_address}')
         reply = AuthResponse.create_access_accept(request=request)
-        reply['Idle-Timeout'] = 86400       # 用户的闲置切断时间
-        reply['User-Name'] = request.username
-        reply['Calling-Station-Id'] = request.mac_address
-        reply['Acct-Interim-Interval'] = ACCOUNTING_INTERVAL
-        reply['State'] = session.session_id.encode()
+        ########
+        # reply['Idle-Timeout'] = 86400       # 用户的闲置切断时间
+        # reply['Acct-Interim-Interval'] = ACCOUNTING_INTERVAL
+        # reply['User-Name'] = request.username
+        # reply['Calling-Station-Id'] = request.mac_address
+        ########
         # reply['Class'] = '\x7f'.join(('EAP-PEAP', session.auth_user.inner_username, session.session_id))   # Access-Accept发送给AC, AC在计费报文内会携带Class值上报
+        reply['State'] = session.session_id.encode()
         log.debug(f'msk: {session.msk}, secret: {reply.secret}, authenticator: {request.authenticator}')
         reply['MS-MPPE-Recv-Key'], reply['MS-MPPE-Send-Key'] = create_mppe_recv_key_send_key(session.msk, reply.secret, request.authenticator)
         reply['EAP-Message'] = struct.pack('!B B H', EapPacket.CODE_EAP_SUCCESS, session.next_eap_id-1, 4)  # eap_id抓包是这样, 不要惊讶!
