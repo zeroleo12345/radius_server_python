1eec502978c530ec53644606e189ba453065d786
zeroleo12345 <zeroleo12345@163.com>
2020-12-01 00:54:41 +0800

    m

--- src/auth/eap_peap_gtc_flow.py
+++ src/auth/eap_peap_gtc_flow.py
@@ -316,4 +316,4 @@ class EapPeapGtcFlow(Flow):
         reply['EAP-Message'] = struct.pack('!B B H', EapPacket.CODE_EAP_SUCCESS, session.next_eap_id-1, 4)  # eap_id抓包是这样, 不要惊讶!
         request.reply_to(reply)
         session.set_reply(reply)
-        # SessionCache.clean(session_id=session.session_id)
+        SessionCache.clean(session_id=session.session_id)
