a15dd86875e201db779b871f3cc459d5ce989267
zeroleo12345 <zeroleo12345@163.com>
2021-02-10 21:50:44 +0800

    m

--- src/processor/auth_processor.py
+++ src/processor/auth_processor.py
@@ -62,7 +62,8 @@ def verify(request: AuthRequest):
                 return EapPeapGtcFlow.authenticate(request=request, auth_user=auth_user)
             else:
                 return EapPeapMschapv2Flow.authenticate(request=request, auth_user=auth_user)
-        return ChapFlow.access_accept(request=request, auth_user=auth_user)
+        elif 'MS-CHAP-Challenge' in request:
+            ChapFlow.access_accept(request=request, auth_user=auth_user)
         raise Exception('can not choose authenticate method')
     except AccessReject:
         Flow.access_reject(request=request, auth_user=auth_user)
