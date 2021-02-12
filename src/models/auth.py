aed1e812d3abec2ef9628129680308069267e26b
zeroleo12345 <zeroleo12345@163.com>
2021-02-12 21:56:10 +0800

    m

--- src/models/auth.py
+++ src/models/auth.py
@@ -9,6 +9,7 @@ class Account(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(255), unique=True, nullable=True)
     password = Column(String(255))
+    radius_password = Column(String(255))
     expired_at = Column(DateTime)
 
     def __repr__(self):
