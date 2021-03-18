6ea2f0c48f4068df9aa0378cb01973f420d0c7a1
zeroleo12345 <zeroleo12345@163.com>
2021-03-18 12:37:52 +0800

    add

--- src/controls/stat.py
+++ src/controls/stat.py
@@ -1,4 +1,5 @@
 import datetime
+import threading
 # 项目库
 from utils.redispool import get_redis
 
@@ -49,3 +50,40 @@ class UserStat(object):
         sub_key = cls.get_sub_key(username, user_mac, ap_mac)
         redis = get_redis()
         redis.hincrby(name=key, key=sub_key, amount=1)
+
+
+class StatThread(object):
+    thread = None
+    is_process_exit = False
+
+    def start(self):
+        """ 开始 """
+        self.thread = threading.Thread(target=self.run)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def stop(self):
+        """ 停止 """
+        if not self.thread:
+            return
+        self.is_process_exit = True
+        self.thread.join(3)
+
+    def run(self):
+        last = 0
+        while 1:
+            if SigTerm.is_term or self.is_process_exit:
+                result = self.release_lock()
+                log.d(f'release lock. result: {result}')
+                raise SystemExit()
+            now = int(time.time())
+            if now - last < (self.expire_time / 2):
+                continue
+            else:
+                last = now
+            key = self.get_key(process_name=self.process_name, worker_id=self.worker_id)
+            is_success = self.get_and_expire(keys=[key], args=[self.pod_uid, self.expire_time])
+            if not is_success:
+                sentry_sdk.capture_message(f'refresh lock fail. key: {key}, pod_uid: {self.pod_uid}')
+            log.t(f'success refresh lock. key: {key}')
+            time.sleep(1)
