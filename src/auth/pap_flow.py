944d01e35f66c9ba068abca1df3003dc18ed6d0a
zeroleo12345 <zeroleo12345@163.com>
2021-03-27 12:57:31 +0800

    add

--- src/auth/pap_flow.py
+++ src/auth/pap_flow.py
@@ -8,31 +8,33 @@ from .flow import Flow, AccessReject
 from loguru import logger as log
 from controls.user import AuthUser
 from models.mac_account import MacAccount
+from auth.session import BaseSession
 
 
 class PapFlow(Flow):
 
     @classmethod
     def authenticate(cls, request: AuthRequest, auth_user: AuthUser):
+        session = BaseSession(auth_user=auth_user)
         # 获取报文
         encrypt_password = request['User-Password'][0]
 
         # 密码解密
         decrypt_password = request.PwCrypt(password=encrypt_password)
-        auth_user.user_password = decrypt_password.decode().split('\x00', 1)[0]
+        session.auth_user.user_password = decrypt_password.decode().split('\x00', 1)[0]
         # User-Name: '5af3ce3a0959'
         # User-Password: '5af3ce3a0959\x00\x00\x00\x00'
 
         # 验证方法
-        if request.user_mac.replace('-', '').lower() == auth_user.outer_username:
-            return cls.mac_auth(request=request, auth_user=auth_user)
+        if request.user_mac.replace('-', '').lower() == session.auth_user.outer_username:
+            return cls.mac_auth(request=request, session=session)
         else:
-            return cls.pap_auth(request=request, auth_user=auth_user)
+            return cls.pap_auth(request=request, session=session)
 
     @classmethod
-    def mac_auth(cls, request: AuthRequest, auth_user: AuthUser):
+    def mac_auth(cls, request: AuthRequest, session: BaseSession):
         # mac Flow: 用户不存在则创建
-        account = MacAccount.get(username=auth_user.outer_username)
+        account = MacAccount.get(username=session.auth_user.outer_username)
         if not account:
             redis = get_redis()
             key = 'enable_mac_authentication'
@@ -43,17 +45,17 @@ class PapFlow(Flow):
             created_at = datetime.datetime.now()
             expired_at = created_at + datetime.timedelta(days=3600)
             MacAccount.create(
-                username=auth_user.outer_username, radius_password=auth_user.user_password, is_enable=True, ap_mac=request.ap_mac,
+                username=session.auth_user.outer_username, radius_password=session.auth_user.user_password, is_enable=True, ap_mac=request.ap_mac,
                 expired_at=expired_at, created_at=created_at,
             )
-            sentry_sdk.capture_message(f'新增放通 MAC 设备, mac_address: {auth_user.user_mac}, ssid: {request.ssid}')
+            sentry_sdk.capture_message(f'新增放通 MAC 设备, mac_address: {session.auth_user.user_mac}, ssid: {request.ssid}')
             redis.delete(key)
 
         return cls.access_accept(request=request, auth_method='MAC-PAP')
 
     @classmethod
-    def pap_auth(cls, request: AuthRequest, auth_user: AuthUser):
-        log.info(f'PAP username: {request.username}, password: {auth_user.user_password}')
+    def pap_auth(cls, request: AuthRequest, session: BaseSession):
+        log.info(f'PAP username: {request.username}, password: {session.auth_user.user_password}')
         return cls.access_accept(request=request, auth_method='PAP')
 
     @classmethod
