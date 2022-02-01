0dbd453b4a6327876cd83bb49e2e932c2da5b121
zeroleo12345 <zeroleo12345@163.com>
2022-02-02 01:23:54 +0800

    m

--- tools/simulator/eap_test/run.sh
+++
@@ -1,3 +0,0 @@
-eapol_test -a 127.0.0.1 -p 1812 -s testing123 -r 0 -c /app/tools/simulator/eap_test/eapol_test.conf.peapv0.mschapv2
-
-eapol_test -a 127.0.0.1 -p 1812 -s testing123 -r 0 -c /app/tools/simulator/eap_test/eapol_test.conf.peapv1.gtc
