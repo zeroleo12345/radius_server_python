807c8cbc628e1413cd77845dcc8309863a0e6fa6
zeroleo12345 <zeroleo12345@163.com>
2022-02-01 22:09:09 +0800

    m

--- data/db/mysql_insert.sql
+++ data/db/mysql_insert.sql
@@ -1,22 +1,20 @@
 CREATE DATABASE `trade` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE `broadband_user` (
+CREATE TABLE `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `openid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nickname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headimgurl` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `platform_id` bigint(20) NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `is_enable` tinyint(1) NOT NULL,
   `role` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expired_at` datetime(6) NOT NULL,
   `created_at` datetime(6) NOT NULL,
   `updated_at` datetime(6) NOT NULL,
-  `expired_at` datetime(6) NOT NULL,
+  `radius_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `openid` (`openid`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  UNIQUE KEY `account_username_cfe70fab_uniq` (`username`),
+  UNIQUE KEY `account_user_id_platform_id_88a2cba9_uniq` (`user_id`,`platform_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=194 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-insert into broadband_user(openid, nickname, headimgurl, username, password, is_enable, role, created_at, updated_at, expired_at) values('fake_openid_1', 'Lyn', 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLX8xKmNq5vZOcmYHUWh7lOEBAKicyCHXPC0yN12yxt6rtDeicR7DCiaYwZ65cTKXNkibk6yp2qsEfXdA/132', 'zhoulixin', 'zhoulixin', 1, 'user', '2018-11-03 09:33:34.660780', '2020-03-28 23:20:54.415637',  '2099-09-01 08:39:25.818951');
-insert into broadband_user(openid, nickname, headimgurl, username, password, is_enable, role, created_at, updated_at, expired_at) values('fake_openid_2', 'Lyn', 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLX8xKmNq5vZOcmYHUWh7lOEBAKicyCHXPC0yN12yxt6rtDeicR7DCiaYwZ65cTKXNkibk6yp2qsEfXdA/132', 'testuser', 'password', 1, 'user', '2018-11-03 09:33:34.660780', '2020-03-28 23:20:54.415637',  '2099-09-01 08:39:25.818951');
+INSERT INTO `account` VALUES (1,1351172106029633536,1,'zhouliying','zhouliying',1,'pay_user','2031-04-17 00:00:00','2018-11-03 00:00:00','2099-09-01 00:00:00','zhouliying')
