c3b39a4e829339a2ecec7da6067a017f7873d849
zeroleo12345 <zeroleo12345@163.com>
2022-02-02 12:21:29 +0800

    add

--- bin/dae.sh
+++ bin/dae.sh
@@ -6,6 +6,6 @@ echo "当前项目目录: $project_root"
 
 export PYTHONPATH=$project_root/src:$PYTHONPATH
 # 环境变量
-export LOG_HEADER="acct"
+export LOG_HEADER="dae"
 
-exec python3 $project_root/src/processor/acct_processor.py
+exec python3 $project_root/src/processor/dae_processor.py
--- bin/shell.sh
+++ bin/shell.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 cd $(dirname "$0")/..
-project_root=$(pwd)     # /root/radius_server
+project_root=$(pwd)
 echo "当前项目目录: $project_root"
 
 export PYTHONPATH=$project_root/src:$PYTHONPATH
 # 环境变量
-export LOG_HEADER="auth"
+export LOG_HEADER="shell"
 
 exec python3
