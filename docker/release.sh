eabd4a1594db089b92d429304994be5be4cb680b
zeroleo12345 <zeroleo12345@163.com>
2022-02-02 16:43:42 +0800

    modify stop and release

--- docker/release.sh
+++ docker/release.sh
@@ -2,3 +2,4 @@
 
 export RESTART_POLICY=unless-stopped; docker-compose up -d auth
 export RESTART_POLICY=unless-stopped; docker-compose up -d acct
+export RESTART_POLICY=unless-stopped; docker-compose up -d dae
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker-compose stop auth acct
+docker-compose stop auth acct dae
