8c33e687f2bf22cc2cf25a84c629c18cc0d715bc
zeroleo12345 <zeroleo12345@163.com>
2022-02-04 11:48:30 +0800

    [ADDED] adjust log format

--- docker/release.sh
+++ docker/release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-export RESTART_POLICY=unless-stopped; docker-compose up -d auth
-export RESTART_POLICY=unless-stopped; docker-compose up -d acct
-export RESTART_POLICY=unless-stopped; docker-compose up -d dae
+export DEBUG=False; export RESTART_POLICY=unless-stopped; docker-compose up -d auth
+export DEBUG=False; export RESTART_POLICY=unless-stopped; docker-compose up -d acct
+export DEBUG=False; export RESTART_POLICY=unless-stopped; docker-compose up -d dae
--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,7 @@ from loguru import logger as log
 SENTRY_DSN = config('SENTRY_DSN', mandatory=False)
 sentry_sdk.init(SENTRY_DSN)
 
+DEBUG = config('DEBUG', default=True, cast='@bool')
 USER_DB_URI = config('USER_DB_URI')     # sqlite:////app/data/db/users.db; mysql://username:password@localhost/test?charset=utf8mb4
 RADIUS_DICTIONARY_DIR = config('RADIUS_DICTIONARY_DIR')
 RADIUS_SECRET = str.encode(config('RADIUS_SECRET'))
@@ -39,15 +40,18 @@ libhostapd = EapCrypto(hostapd_library_path=HOSTAPD_LIBRARY, ca_cert_path=CA_CER
 LOG_HEADER = config('LOG_HEADER', default='')
 LOG_DIR = config('LOG_DIR', default='')
 LOG_LEVEL = config('LOG_LEVEL')
+LOG_FILE_FORMAT = config('LOG_FILE_FORMAT', default="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {message}")
 # 初始化日志
 log.remove()    # workaround: https://github.com/Delgan/loguru/issues/208
-log.add(sys.stderr, level=LOG_LEVEL)
+if DEBUG:
+    log_console_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <level>{message}</level>"
+    log.add(sys.stderr, level=LOG_LEVEL, format=log_console_format)
 if LOG_DIR and LOG_HEADER:
     log.info('enable log to file')
-    log.add(os.path.join(LOG_DIR, LOG_HEADER + '_{time:YYYYMMDD_HHmmss_SSSSSS}.log'), rotation='00:00', level=LOG_LEVEL)
+    log.add(os.path.join(LOG_DIR, LOG_HEADER + '_{time:YYYYMMDD_HHmmss_SSSSSS}.log'), rotation='00:00', level=LOG_LEVEL, format=LOG_FILE_FORMAT)
 else:
     log.info('close log to file')
-log.warning(f'Log parameter. Level: {LOG_LEVEL}, Header: {LOG_HEADER}, Directory: {LOG_DIR}')
+log.info(f'Log parameter. Level: {LOG_LEVEL}, Header: {LOG_HEADER}, Directory: {LOG_DIR}')
 
 
 def cleanup():
