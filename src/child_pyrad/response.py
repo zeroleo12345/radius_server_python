f1188bf3ec26cee1b336c64b511fa1a2fe81b3b8
zeroleo12345 <zeroleo12345@163.com>
2022-03-13 21:01:32 +0800

    m

--- src/child_pyrad/response.py
+++ src/child_pyrad/response.py
@@ -30,12 +30,6 @@ class AuthResponse(AuthPacket):
         # 用户的闲置切断时间
         reply['Idle-Timeout'] = 86400
         reply['Acct-Interim-Interval'] = ACCOUNTING_INTERVAL
-        # 上载速度. 用户到NAS的峰值速率. 单位是bps:(即1/8字节每秒). 此参数对PPPoE用户有效, wlan用户无效
-        reply['H3C-Input-Peak-Rate'] = int(12 * 1000000)
-        reply['H3C-Input-Average-Rate'] = int(10 * 1000000)
-        # 下载速度. NAS到用户的峰值速率. 单位是bps:(即1/8字节每秒). 此参数对PPPoE用户有效, wlan用户无效
-        reply['H3C-Output-Peak-Rate'] = int(35 * 1000000)
-        reply['H3C-Output-Average-Rate'] = int(30 * 1000000)
         if request.username == 'zhouliying':
             # 1M bit = 1000000
             # 下载速度. NAS到用户的峰值速率. 单位是bps:(即1/8字节每秒). 此参数对PPPoE用户有效, wlan用户无效
@@ -46,8 +40,16 @@ class AuthResponse(AuthPacket):
             reply['H3C-Input-Average-Rate'] = int(3 * 1000000)
             # User Profile 适用于wlan和PPPoE用户. 当AC profile disable时, 会连不上WIFi
             # reply['Filter-Id'] = f'pay_user_4m'
-        if request.auth_protocol == PacketProtocol.CHAP_PROTOCOL:
+        if request.auth_protocol in [PacketProtocol.CHAP_PROTOCOL, PacketProtocol.PAP_PROTOCOL, PacketProtocol.MAC_PROTOCOL]:
             reply['Class'] = uuid4().hex.encode()
+            # 上载速度. 用户到NAS的峰值速率. 单位是bps:(即1/8字节每秒). 此参数对PPPoE用户有效, wlan用户无效
+            reply['H3C-Input-Peak-Rate'] = int(12 * 1000000)
+            reply['H3C-Input-Average-Rate'] = int(10 * 1000000)
+            # 下载速度. NAS到用户的峰值速率. 单位是bps:(即1/8字节每秒). 此参数对PPPoE用户有效, wlan用户无效
+            reply['H3C-Output-Peak-Rate'] = int(35 * 1000000)
+            reply['H3C-Output-Average-Rate'] = int(30 * 1000000)
+        if request.auth_protocol in [PacketProtocol.EAP_PEAP_MSCHAPV2_PROTOCOL, PacketProtocol.EAP_PEAP_GTC_PROTOCOL, PacketProtocol.MSCHAPV2_PROTOCOL]:
+            reply['Filter-Id'] = f'pay_user_50m'
         return reply
 
     @classmethod
