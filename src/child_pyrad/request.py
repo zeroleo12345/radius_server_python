36ec0c90cba87c54d7777c8aa15dde16bc100edd
zeroleo12345 <zeroleo12345@163.com>
2022-09-06 19:16:24 +0800

    [UPDATE] set user_mac is optional in auth

--- src/child_pyrad/request.py
+++ src/child_pyrad/request.py
@@ -20,9 +20,11 @@ class AuthRequest(AuthPacket):
         # 报文提取
         # self['Service-Type'][0] 和 self['Service-Type'][1] 分别对应字典 dictionary.pyrad 里面 VALUE Service-Type Call-Check 10 的第1个和第2个值
         self.username = self['User-Name'][0]
-        self.user_mac = self['Calling-Station-Id'][0]
         self.nas_name = self['NAS-Identifier'][0]
         self.nas_ip = self['NAS-IP-Address'][0]
+        # optional:
+        default_element = ('', 0)
+        self.user_mac = self.get('Calling-Station-Id', default_element)[0]
 
         self.ssid = ''
         self.ap_mac = ''
@@ -81,12 +83,12 @@ class AcctRequest(AcctPacket):
                                  code=self.code, id=0, secret=secret, authenticator=None, dict=dict, packet=packet)
         self.socket, self.address = socket, address
         # 报文提取
-        default_element = ('', 0)
         self.username = self['User-Name'][0]
         self.nas_name = self['NAS-Identifier'][0]
         self.nas_ip = self['NAS-IP-Address'][0]
         self.iut = self['Acct-Status-Type'][0]   # I,U,T包. Start-1; Stop-2; Interim-Update-3; Accounting-On-7; Accounting-Off-8;
-        #
+        # optional:
+        default_element = ('', 0)
         self.user_mac = self.get('Calling-Station-Id', default_element)[0]
         self.auth_class = self.get('Class', default_element)[0]
 
