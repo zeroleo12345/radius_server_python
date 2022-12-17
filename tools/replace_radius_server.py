057530a510d89215c8092ff45b6f037d53a27b7b
zeroleo12345 <zeroleo12345@163.com>
2022-12-18 00:03:13 +0800

    m

---
+++ tools/replace_radius_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+radius_server_ip = sys.argv[1]
+print(f"\nRadius Server IP: {radius_server_ip}\n")
+
+msg = f"""
+radius dynamic-author client trusted ip {radius_server_ip}
+acl number 2000
+ rule 50 permit source {radius_server_ip} 0
+quit
+acl number 3000
+ rule 50 permit tcp source {radius_server_ip} 0 destination-port eq telnet
+quit
+radius scheme eap_radius_scheme
+ primary authentication {radius_server_ip} probe username user_probe interval 12
+ primary accounting {radius_server_ip} probe username user_probe interval 12
+quit
+radius scheme eapoff_radius_scheme
+ primary authentication {radius_server_ip} probe username user_probe interval 12
+ primary accounting {radius_server_ip} probe username user_probe interval 12
+quit
+"""
+
+print("################# 执行的语句 ########################")
+print(msg)
