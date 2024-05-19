cae3552681792288938d3813a1b71574b11d7ad9
zeroleo12345 <zeroleo12345@163.com>
2024-05-19 19:44:39 +0800

    afix

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,13 +3,13 @@ from playhouse.db_url import parse
 # 项目库
 from settings import DB_URI
 
-# {'database': 'trade', 'user': 'root', 'password': 'root', 'host': 'mysql', 'charset': 'utf8mb4'}
+# {'database': 'trade', 'user': 'root', 'host': 'mysql', 'passwd': 'root'}
 db_param: dict = parse(DB_URI)
 
 db = PooledMySQLDatabase(
     database=db_param['database'],
     user=db_param['user'],
-    password=db_param['password'],
+    password=db_param['passwd'],
     host=db_param['host'],
     charset='utf8mb4',
     max_connections=20,
