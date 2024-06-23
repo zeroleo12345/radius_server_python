edec50d818bf39b4624733bb4c9a6168c8be98f9
Lyn <13072815+zeroleo12345@users.noreply.github.com>
2024-06-23 16:03:21 +0800

    [ADDED] rename DB_URI (#48)

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 # 项目库
-from settings import USER_DB_URI
+from settings import DB_URI
 
 
-engine = create_engine(USER_DB_URI, pool_recycle=3600, echo=False)   # echo: 控制打印sql; pool_recycle: MySQL server has gone away
+engine = create_engine(DB_URI, pool_recycle=3600, echo=False)   # echo: 控制打印sql; pool_recycle: MySQL server has gone away
 metadata = MetaData(bind=engine)
 Base = declarative_base(bind=engine)
 Session = sessionmaker(bind=engine)
