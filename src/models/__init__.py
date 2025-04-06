675829e53c7563b49d723d5be165a9a5c26cc11f
zeroleo12345 <zeroleo12345@163.com>
2025-04-06 09:50:46 +0800

    fix

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
-from playhouse.pool import PostgresqlDatabase
+from playhouse.pool import PostgresqlDatabase, PooledPostgresqlDatabase
 from playhouse.db_url import parse
 from playhouse.shortcuts import ReconnectMixin
 # 项目库
@@ -7,11 +7,16 @@ from settings import DATABASE_URI
 # {'database': 'trade', 'user': 'root', 'host': 'pg', 'passwd': 'root'}
 db_param: dict = parse(DATABASE_URI)
 
-class ReconnectPooledMySQLDatabase(ReconnectMixin, PostgresqlDatabase):
+
+# class ReconnectPooledPostgresDatabase(ReconnectMixin, PooledPostgresqlDatabase):
+#    pass
+
+class ReconnectPostgresDatabase(ReconnectMixin, PostgresqlDatabase):
     # 使用 pgbouncer 后,不需使用 PooledPostgresqlDatabase
     pass
 
-db = ReconnectPooledMySQLDatabase(
+
+db = ReconnectPostgresDatabase(
     database=db_param['database'],
     user=db_param['user'],
     password=db_param['passwd'],
