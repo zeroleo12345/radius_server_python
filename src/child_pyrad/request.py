dbfa714d36ee8e5c9cad30ffe1712fc2a6b5aae0
zeroleo12345 <zeroleo12345@163.com>
2025-05-04 21:00:19 +0800

    fix

--- src/child_pyrad/request.py
+++ src/child_pyrad/request.py
@@ -107,10 +107,12 @@ class AcctRequest(AcctPacket):
         self.nas_ip = self['NAS-IP-Address'][0]    # 如果获取自报文字段 self['NAS-IP-Address'][0], 会出现ip更新不及时, 与真实IP不一致的问题
         self.iut = self['Acct-Status-Type'][0]   # I,U,T包. Start-1; Stop-2; Interim-Update-3; Accounting-On-7; Accounting-Off-8;
         self.session_time = self['Acct-Session-Time'][0]    # 秒
-        self.upload_gigabytes = self['Acct-Input-Gigawords'][0]
-        self.download_gigabytes = self['Acct-Output-Gigawords'][0]
-        self.upload_bytes = self['Acct-Input-Octets'][0]
-        self.download_bytes = self['Acct-Output-Octets'][0]
+        #
+        default_string = (0, 0)
+        self.upload_gigabytes = self.get('Acct-Input-Gigawords', default_string)[0]
+        self.download_gigabytes = self.get('Acct-Output-Gigawords', default_string)[0]
+        self.upload_bytes = self.get('Acct-Input-Octets', default_string)[0]
+        self.download_bytes = self.get('Acct-Output-Octets', default_string)[0]
         # optional:
         default_string = ('', 0)
         self.user_mac = self.get('Calling-Station-Id', default_string)[0]
