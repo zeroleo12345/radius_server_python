2aa008ff9fd6a50f95aa764900c2768a1cf82c7d
zeroleo12345 <zeroleo12345@163.com>
2025-05-05 00:11:57 +0800

    fix

--- src/child_pyrad/request.py
+++ src/child_pyrad/request.py
@@ -23,7 +23,7 @@ class AuthRequest(AuthPacket):
         """
         try:
             init_packet_from_receive(super(), code=self.code, id=0, secret=secret, authenticator=None, dict=dict, packet=packet)
-            # access-request 需要 Message-Authenticator 字段验证报文合法性
+            # access-request 需要 Message-Authenticator 字段验证报文合法性; 报文头Authenticator字段是随机生成的
             if not self.VerifyAuthRequest():
                 log.warning(f'VerifyAuthRequest failed from address: {address}')
         except Exception as e:
