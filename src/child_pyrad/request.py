ae890eaac84de7806f0a11f7fa33582579d0d379
zeroleo12345 <zeroleo12345@163.com>
2025-05-05 00:01:40 +0800

    fix

--- src/child_pyrad/request.py
+++ src/child_pyrad/request.py
@@ -23,7 +23,8 @@ class AuthRequest(AuthPacket):
         """
         try:
             init_packet_from_receive(super(), code=self.code, id=0, secret=secret, authenticator=None, dict=dict, packet=packet)
-            assert self.VerifyAuthRequest()
+            if not self.VerifyAuthRequest():
+                log.warning(f'VerifyAuthRequest failed from address: {address}')
         except Exception as e:
             raise PacketError(str(e))
         self.socket, self.address = socket, address
