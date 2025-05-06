d14a14b270aefaf1f9cbedb3c88d85d54ee0ffcb
zeroleo12345 <zeroleo12345@163.com>
2025-05-06 21:34:43 +0800

    fix

--- src/utils/prometheus.py
+++ src/utils/prometheus.py
@@ -1,8 +1,13 @@
 import json
 import requests
+# 项目库
+from utils.config import config
 
 
 class Prometheus(object):
-    # curl -d 'foo{bar="baz"} 111 1746457233000' -X POST 'http://localhost:8428/prometheus/api/v1/import/prometheus'
-    pass
+    ENDPOINT = config('PROMETHEUS_ENDPOINT', default='http://metric:8428/prometheus/api/v1/import/prometheus')
 
+    @classmethod
+    def push_metric(cls, receiver_id: str, text: str):
+        # curl -d 'foo{bar="baz"} 111 1746457233000' -X POST http://metric:8428/prometheus/api/v1/import/prometheus
+        pass
