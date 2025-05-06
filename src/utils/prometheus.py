00dc787a27fa8f7d25f9d5eff9f310a3e4d91730
zeroleo12345 <zeroleo12345@163.com>
2025-05-06 21:24:46 +0800

    fix

---
+++ src/utils/prometheus.py
@@ -0,0 +1,8 @@
+import json
+import requests
+
+
+class Prometheus(object):
+    # curl -d 'foo{bar="baz"} 111 1746457233000' -X POST 'http://localhost:8428/prometheus/api/v1/import/prometheus'
+    pass
+
