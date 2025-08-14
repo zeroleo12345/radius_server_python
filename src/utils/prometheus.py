27fd948d3fe317d851367b6633a6ab25835e385f
zeroleo12345 <zeroleo12345@163.com>
2025-08-14 22:47:24 +0800

    add

--- src/utils/prometheus.py
+++ src/utils/prometheus.py
@@ -10,6 +10,14 @@ class Prometheus(object):
     def push_metric(cls, metrics: list):
         """
         curl -d 'foo{bar="baz"} 111 1746457233000' -X POST http://metric:8428/prometheus/api/v1/import/prometheus
+
+        :param metrics:
+            [
+                upload_kb{username="111"} 1024 1755182746',
+                download_kb{username="222"} 10240 1755182746',
+                session_time{username="333"} 86400 1755182746',
+            ]
+        :return:
         """
         data = '\n'.join(metrics)
         headers = {
