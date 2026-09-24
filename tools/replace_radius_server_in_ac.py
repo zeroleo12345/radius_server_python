598d6d06c024c404e4f097f9c81b4c6215e5fee4
zeroleo12345 <zeroleo12345@163.com>
2026-09-24 18:44:59 +0800

    update replace_radius_server_in_ac.py

--- tools/replace_radius_server_in_ac.py
+++ tools/replace_radius_server_in_ac.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
 
 import sys
+if len(sys.argv) <= 2:
+    print("[E] 需要2个参数: IPv4 和 IPv6")
+    exit()
 radius_server_ip = sys.argv[1]
+radius_server_ipv6 = sys.argv[2]
 print(f"\nRadius Server IP: {radius_server_ip}\n")
+print(f"\nRadius Server IPv6: {radius_server_ipv6}\n")
 
 msg = f"""
-radius dynamic-author client trusted ip {radius_server_ip}
-acl number 3001
- undo rule 50
- rule 50 permit tcp source {radius_server_ip} 0 destination-port eq telnet
+# MSR3600
+acl number 2422
+    undo rule 99
+    rule 99 permit source {radius_server_ip} 0
 quit
-radius scheme eap_radius_scheme
- primary authentication {radius_server_ip} probe username user_probe interval 12
- primary accounting {radius_server_ip} probe username user_probe interval 12
+
+acl ipv6 number 2622
+    undo rule 99
+    rule 99 permit source {radius_server_ipv6}/128
 quit
-radius scheme eapoff_radius_scheme
- primary authentication {radius_server_ip} probe username user_probe interval 12
- primary accounting {radius_server_ip} probe username user_probe interval 12
+
+radius scheme eap_and_mac_radius_server
+    primary authentication {radius_server_ipv6} test-profile user_probe
+    primary accounting {radius_server_ip}
+quit
+
+radius dynamic-author server
+    client ip {radius_server_ip} key simple testing123
+    client ipv6 {radius_server_ipv6} key simple testing123
 quit
 
 save f
+
+
+# MSR2600
+TODO
 """
 
 print("################# 执行的语句 ########################")
